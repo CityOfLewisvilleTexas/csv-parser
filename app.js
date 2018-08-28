718cd723f030f1966d401235539251eb6aa55a38
colton-wood <cwood@cityoflewisville.com>
2018-08-28 11:11:37 -0500

    added "stepper" wizard and support for filtered columns

--- app.js
+++ app.js
@@ -6,24 +6,94 @@ var app = new Vue({
 
     // vars
     data: {
+        stepper: 1,
+        verifiedHeaders: [],
+        processing: false,
+
+
         filename: '',
+        filesize: 0,
         fileAsArray: [],
         detailsVisible: true,
         filetype: '',
         headers: []
     },
 
+    computed: {
+        verifiedHeadersFormatted: function() {
+            return this.verifiedHeaders.map(function(h) {
+                return {
+                    text: h,
+                    value: h
+                }
+            })
+        },
+
+        fileAsArrayFormatted: function() {
+            if (this.filetype == 'csv') {
+                return this.fileAsArray.slice(1).map(function(row) {
+                    var obj = {}
+                    for (var prop in this.fileAsArray[0]) {
+                        // verified only
+                        if (this.verifiedHeaders.indexOf(this.fileAsArray[0][prop]) != -1)
+                            obj[this.fileAsArray[0][prop]] = row[prop]
+                    }
+                    return obj
+                }.bind(this))
+            } else return this.fileAsArray.map(function(row) {
+                var obj = {}
+                for (var prop in this.fileAsArray[0]) {
+                    // verified only
+                    if (this.verifiedHeaders.indexOf(prop) != -1)
+                        obj[prop] = row[prop]
+                }
+                return obj
+            }.bind(this))
+        }
+    },
+
     // functions
     methods: {
+
+        fake: function() {
+            this.filename = 'fake'
+            this.filetype = 'xlsx'
+            this.filesize = '1 KB'
+            this.fileAsArray = [
+                { fname: 'colton', lname: 'wood', email: 'cwood@col.com' },
+                { fname: 'elizabeth', lname: 'mayes', email: 'emayes@col.com' },
+                { fname: 'jason', lname: 'kirkland', email: 'jkirkland@col.com' }
+            ]
+            this.headers = [
+                { text: 'fname', value: 'fname' },
+                { text: 'lname', value: 'lname' },
+                { text: 'email', value: 'email' }
+            ]
+            this.verifiedHeaders = this.headers.map(function(h) { return h.text })
+            this.stepper = 2
+        },
 
         pickFile: function() {
             this.$refs.myFiles.click()
         },
 
+        updateFileName: function() {
+            this.filename = this.$refs.myFiles.value.split('\\')[this.$refs.myFiles.value.split('\\').length - 1]
+            var fsz = this.$refs.myFiles.files[0].size
+            var sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB']
+            if (fsz == 0) this.filesize = '0 Bytes'
+            else {
+                var i = parseInt(Math.floor(Math.log(fsz) / Math.log(1024)))
+                this.filesize = (fsz / Math.pow(1024, i)).toFixed(2) + ' ' + sizes[i]
+            }
+
+
+        },
+
         handleFiles: function() {
             if (window.FileReader) {
-                this.filename = this.$refs.myFiles.value.split('\\')[this.$refs.myFiles.value.split('\\').length - 1]
                 this.fileAsArray = []
+                this.processing = true
                 if (this.filename.slice(-3) == 'csv') {
                     this.filetype = 'csv'
                     this.csvToJson()
@@ -69,6 +139,17 @@ var app = new Vue({
                 lines.push(tarr[0].split(','))
             }
             this.fileAsArray = lines
+            this.headers = []
+            this.verifiedHeaders = []
+            for (var prop in this.fileAsArray[0]) {
+                this.headers.push({
+                    text: this.fileAsArray[0][prop],
+                    value: this.fileAsArray[0][prop]
+                })
+            }
+            this.verifiedHeaders = this.headers.map(function(h) { return h.value })
+            this.processing = false
+            this.stepper = 2
         },
 
         // alert any errors
@@ -103,6 +184,8 @@ var app = new Vue({
                     // Here is your object
                     var XL_row_object = XLSX.utils.sheet_to_json(ws, { defval: '', raw: true });
                     this.fileAsArray = XL_row_object
+                    this.headers = []
+                    this.verifiedHeaders = []
                     for (var prop in this.fileAsArray[0]) {
                         if (this.fileAsArray[0].hasOwnProperty(prop)) {
                             this.headers.push({
@@ -111,6 +194,9 @@ var app = new Vue({
                             })
                         }
                     }
+                    this.verifiedHeaders = this.headers.map(function(h) { return h.value })
+                    this.processing = false
+                    this.stepper = 2
                 }.bind(this))
             }.bind(this)
 
