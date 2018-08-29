1c7e18cf70f8bab24009a22cd4b0dc23ca90f4cd
colton-wood <cwood@cityoflewisville.com>
2018-08-29 13:19:26 -0500

    add setup page for creator

---
+++ setup/app.js
@@ -0,0 +1,19 @@
+"use strict";
+
+// Vue!
+var app = new Vue({
+    el: "#app",
+
+    // vars
+    data: {
+        stepper: 1
+    },
+
+    // start here
+    mounted: function() {
+    },
+
+    // functions
+    methods: {
+    }
+})
