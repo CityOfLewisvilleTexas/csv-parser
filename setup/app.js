1e1d7a24de747afd29522ac7c37a36aad2d5c021
colton-wood <cwood@cityoflewisville.com>
2018-08-29 13:35:57 -0500

    added table selection (not live data)

--- setup/app.js
+++ setup/app.js
@@ -6,14 +6,35 @@ var app = new Vue({
 
     // vars
     data: {
-        stepper: 1
+        stepper: 1,
+        tables: [],
+        isLoading: {
+            tables: false
+        },
+        selected: {
+            table: ''
+        }
     },
 
     // start here
     mounted: function() {
+        this.fetchTables()
     },
 
     // functions
     methods: {
+
+        // fetch all tables in [CSVUploadApp]
+        fetchTables: function() {
+            if (this.isLoading.tables===false) {
+                this.isLoading.tables = true
+
+                // axios('fetch tables')
+                this.handleTables(['table1', 'table2', 'table3', 'table4', 'table5'])
+            }
+        },
+        handleTables: function(res) {
+            this.tables = res
+        }
     }
 })
