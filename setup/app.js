89968b06555866039a52a035c9cb924ac865b15a
colton-wood <cwood@cityoflewisville.com>
2018-08-29 13:59:25 -0500

    added mask selection (no live data)

--- setup/app.js
+++ setup/app.js
@@ -8,11 +8,22 @@ var app = new Vue({
     data: {
         stepper: 1,
         tables: [],
+        masks: [],
+        mask: '',
+
+
         isLoading: {
-            tables: false
+            tables: false,
+            masks: false
         },
         selected: {
             table: ''
+        }
+    },
+
+    watch: {
+        stepper: function() {
+            if (this.stepper === 2) this.fetchMasks()
         }
     },
 
@@ -30,11 +41,50 @@ var app = new Vue({
                 this.isLoading.tables = true
 
                 // axios('fetch tables')
-                this.handleTables(['table1', 'table2', 'table3', 'table4', 'table5'])
+                setTimeout(() => {
+                    this.handleTables(['table1', 'table2', 'table3', 'table4', 'table5'])
+                }, 500)
             }
         },
         handleTables: function(res) {
             this.tables = res
+            this.isLoading.tables = false
+        },
+
+        // fetch all masks
+        fetchMasks: function() {
+            if (this.isLoading.masks===false) {
+                this.isLoading.masks = true
+
+                // axios('fetch masks')
+                setTimeout(() => {
+                    this.handleMasks(['mask1', 'mask2', 'mask3', 'mask4', 'mask5'])
+                }, 500)
+            }
+        },
+        handleMasks: function(res) {
+            this.masks = res
+            this.isLoading.masks = false
+        },
+
+        // generates random string
+        generateRandomMask: function() {
+            var text = ""
+            var possible = "abcdefghijklmnopqrstuvwxyz"
+            for (var i = 0; i < 6; i++) text += possible.charAt(Math.floor(Math.random() * possible.length))
+            return text;
+        },
+
+        // checks if provided mask is unique
+        isUniqueMask: function(mask) {
+            return this.masks.indexOf(mask) == -1
+        },
+
+        // randomize mask
+        fillWithRandomMask: function(context) {
+            this.mask = this.generateRandomMask()
+            while(!this.isUniqueMask(this.mask))
+                this[context] = this.generateRandomMask()
         }
     }
 })
